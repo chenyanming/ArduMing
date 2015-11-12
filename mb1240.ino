322f59af5f809f1fa18a9ac888eb039075239a58
damonchan <elecming@gmail.com>
2015-11-12 21:43:23 +0800

    Get sonar value and Z velocity

--- mb1240.ino
+++ mb1240.ino
@@ -4,10 +4,13 @@ float _sonar_altitude = 0;
 float tmp_sonar_altitude[10];
 float _sonar_mode_altitude = 0;
 int alt_mode_count = 0;
+long sonar_current_time = 0, sonar_previous_time = 0;
 
 Kalman kalmanS;
 uint32_t timerS;
 float kal_sonar = 0;
+float pre_kal_sonar = 0;
+float Vel_sonar = 0;
 void sonar_setup()
 {
 	// Kalman init
@@ -18,8 +21,33 @@ void sonar_setup()
 
 void sonar_get() {
 	_sonar_altitude = analogRead(analogPin);    // read the input pin
-	// Serial.println(_sonar_altitude);             // debug value
 	// delay(100);
+
+	// Kalman Fiter
+	double dtS = (double)(micros() - timerS) / 1000000;
+	timerS = micros();
+
+	kal_sonar = kalmanS.getAngle(_sonar_altitude, AcceZ_W, dtS);
+
+	// Velocity of sonar
+	sonar_current_time = millis();
+
+	Vel_sonar = (constrain((kal_sonar - pre_kal_sonar), -20, 20) * 1000)  /  (sonar_current_time - sonar_previous_time);
+
+	// LPF
+	static int sonar_offset = 0;
+	sonar_offset -= sonar_offset / 8;
+	sonar_offset += Vel_sonar;
+	Vel_sonar = sonar_offset / 8;
+
+	// Dead band
+	if (abs(Vel_sonar) < 3) {
+		Vel_sonar = 0;
+	}
+
+	// Vel_sonar = kalmanS.getAngle(Vel_sonar, AcceZ_W, dtS);
+	pre_kal_sonar = kal_sonar;
+	sonar_previous_time = sonar_current_time;
 }
 
 void sonar_mode() {
@@ -41,14 +69,34 @@ void sonar_mode() {
 				}
 			}
 		}
+		/*
 		double dtS = (double)(micros() - timerS) / 1000000;
 		timerS = micros();
 		// _sonar_mode_altitude = (tmp_sonar_altitude[4] + tmp_sonar_altitude[5]) / 2;
 		_sonar_mode_altitude = tmp_sonar_altitude[2];
-		kal_sonar = kalmanS.getAngle(_sonar_mode_altitude, Az, dtS);
+
+		kal_sonar = kalmanS.getAngle(_sonar_mode_altitude, AcceZ_W, dtS);
+
 		// for (int i = 0; i < 10; i++) {
 		// 	average_altitude += tmp_altitude[i];
 		// }
 		// average_altitude /= 10;
+		sonar_current_time = millis();
+		Vel_sonar = ((kal_sonar - pre_kal_sonar)*1000)  /  (sonar_current_time - sonar_previous_time);
+		// LPF
+		static int sonar_offset = 0;
+		sonar_offset -= sonar_offset / 8;
+		sonar_offset += Vel_sonar;
+		Vel_sonar = sonar_offset / 8;
+		// Dead band
+		if(abs(Vel_sonar) == 1){
+			Vel_sonar = 0;
+		}
+
+		// Vel_sonar = kalmanS.getAngle(Vel_sonar, AcceZ_W, dtS);
+		pre_kal_sonar = kal_sonar;
+		sonar_previous_time = sonar_current_time;
+		*/
+
 	}
 }
